fdae70f0f5f952ec7131a656fe5763915b9d3656
Majed Hariri <majedoh@gmail.com>
2022-10-15 15:20:31 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -14,28 +14,27 @@ let package = Package(
         .package(
             name: "AWSiOSSDKV2",
             url: "https://github.com/aws-amplify/aws-sdk-ios-spm.git",
-            .upToNextMinor(from: "2.27.0")
+            .upToNextMajor(from: "2.27.0")
         ),
         .package(
             name: "AppSyncRealTimeClient",
             url: "https://github.com/aws-amplify/aws-appsync-realtime-client-ios.git",
-            from: "1.8.0"
+            .upToNextMajor(from: "1.8.0")
         ),
         .package(
             url: "https://github.com/stephencelis/SQLite.swift.git",
-            from: "0.12.0"
+            .upToNextMajor(from: "0.12.0")
         ),
         .package(
             name: "Reachability",
             url: "https://github.com/ashleymills/Reachability.swift.git",
-            from: "5.0.0"
+            .upToNextMajor(from: "5.0.0")
         )
     ],
     targets: [
         .target(
             name: "AWSAppSync",
             dependencies: [
-                
                 .product(name: "SQLite", package: "SQLite.swift"),
                 .product(name: "AppSyncRealTimeClient", package: "AppSyncRealTimeClient"),
                 .product(name: "Reachability", package: "Reachability"),
